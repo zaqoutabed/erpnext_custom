07ee5ad7c357c0f81a9260da2d5756d13cecfae2
zaqoutabed <abdullahzaqout20@gmail.com>
2023-02-21 18:30:07 +0200

    check payments if is POS

--- erpnext_custom/doc_events.py
+++ erpnext_custom/doc_events.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.utils import time_diff_in_hours
+from frappe.utils import time_diff_in_hours, flt
 
 def attendance_validate(doc, method):
     if not doc.check_in or not doc.check_out:
@@ -12,12 +12,25 @@ def attendance_validate(doc, method):
     doc.hours = working_hours
 
 def sales_invoice_validate(doc, method):
+    validate_pos_payments(doc)
+    update_sales_invoice_remarks(doc)
+    
+
+def validate_pos_payments(doc):
+    if doc.is_pos == 0: return
+    if len(doc.payments) <= 0:
+        frappe.throw("Please add at least one payment")
+    for payment in doc.payments:
+        if flt(payment.amount) <= 0:
+            frappe.throw("Payment amount must be greater than 0")
+
+def update_sales_invoice_remarks(doc):
     invoice_notes = []
     for item in doc.items:
         if item.notes and len(item.notes) > 0:
             item.notes = get_clean_notes(item.notes)
             invoice_notes.append(item.notes)
-
+    if len(invoice_notes) <= 0: return
     invoice_notes = "\n".join(invoice_notes)
     if len(invoice_notes) > 0:
         doc.remarks = invoice_notes
