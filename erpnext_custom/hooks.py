6eaac766e5f4f7257b1fd397257f499e780ff724
zaqoutabed <abdullahzaqout20@gmail.com>
2023-02-26 23:33:14 +0200

    route to work order from stock item

--- erpnext_custom/hooks.py
+++ erpnext_custom/hooks.py
@@ -34,7 +34,8 @@ app_license = "MIT"
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 doctype_js = {
     "Quotation" : "public/js/quotation.js",
-    "Sales Invoice" : "public/js/sales_invoice.js"
+    "Sales Invoice" : "public/js/sales_invoice.js",
+    "Item" : "public/js/item.js",
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
